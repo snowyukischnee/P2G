359bd895b584e8f33bb0d3c10812c50e7edb6932
misaka8912 <tuannhse04791@fpt.edu.vn>
2017-06-24 23:52:19 +0700

    completed phase commit function

--- gameModules/warfare/utils/roomCommit.js
+++ gameModules/warfare/utils/roomCommit.js
@@ -1,29 +1,59 @@
-module.exports = (room) => {
-    room.lock = true;
-    let guard = [[]], offensive = [[]];
-    guard.length = offensive.length = room.players.length;
-    for (let i = 0; i < guard.length; i++) guard[i] = offensive[i] = room.players.length;
-    let name = {};
-    for (let index = 0; index < room.players.length; index++) name[room.players[index].id] = index;
-    console.log(name);
-    for (let index = 0; index < room.players.length; index++) {
-        for (let i = 0; i < 3; i++) {
-            guard[index][i] = null;
-            offensive[index][i] = null;
-            if (room.players[index].action[i] != null) {
-                console.log(room.players[index].action[i].target);
-                if (room.players[index].action[i].type == 0) guard[index][i] = name[room.players[index].action[i].target];
-                else if (room.players[index].action[i].type == 1) offensive[index][i] = name[room.players[index].action[i].target];
+function update(room, guard, offensive, name) {
+    let diff = []; diff.length = room.players.length;
+    for (let i = 0; i < diff.length; i++) diff[i] = 0;
+    for (let i = 0; i < room.players.length; i++) {
+        for (let j = 0; j < room.players.length; j++) {
+            if (guard[i][j] == true && offensive[i][j] == true) {
+                diff[i] -= 1;
+                diff[j] -= 1;
+            } else if (guard[i][j] == true && offensive[i][j] == false) {
+                diff[i] -= 1;
+            } else if (guard[i][j] == false && offensive[i][j] == true) {
+                diff[i] -= 3;
+                diff[j] -= 1;
             } else {
-                guard[index][i] = 0;
-                offensive[index][i] = 0;
+                // not changed
             }
         }
     }
-    let diffPts = [];
+    console.log(diff);
     for (let index = 0; index < room.players.length; index++) {
-        //change player hp
+       room.players[index].hp += diff[index];
     }
-    room.lock = false;
+    return room;
+}
+
+function initTable(h, w) {
+    let table = [];
+    table.length = h;
+    for (let i = 0; i < h; i++) {
+        table[i] = [];
+        table[i].length = w;
+        for (let j = 0; j < w; j++) {
+            table[i][j] = false;
+        }
+    }
+    return table
+}
+
+module.exports = (room) => {
+    room.lock = true; // lock state
+    //-----------------init----------------------------------
+    let guard = initTable(room.players.length, room.players.length), offensive = initTable(room.players.length, room.players.length);
+    let name = {};
+    for (let index = 0; index < room.players.length; index++) {
+        name[room.players[index].id] = index;
+    }
+    //fill attack defense tables
+    for (let index = 0; index < room.players.length; index++) {
+        for (let i = 0; i < 3; i++) {
+            if (room.players[index].action[i] != null) {
+                if (room.players[index].action[i].type == 0) guard[index][name[room.players[index].action[i].target]] = true;
+                else if (room.players[index].action[i].type == 1) offensive[index][name[room.players[index].action[i].target]] = true;
+            }
+        }
+    }
+    room = update(room, guard, offensive, name);
+    room.lock = false; // unlock state
     return room;
 }
